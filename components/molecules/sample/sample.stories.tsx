80644925296a6f665011032953082b3c6635921d
yoshida <yuya_yoshida@forcia.com>
2023-10-04 13:53:18 +0900

    :fix:: story as story type

--- components/molecules/sample/sample.stories.tsx
+++ components/molecules/sample/sample.stories.tsx
@@ -1,3 +1,6 @@
+import {Story} from "@ladle/react"
 import {Sample} from "./sample"
 
-export const SampleStory = <Sample name="waiyade" />;
+export const SampleStory:Story = () => <Sample name="waiyade" />;
+
+export const SampleStory2:Story = () => "uouo";
